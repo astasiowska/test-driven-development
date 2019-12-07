d4c9c331847b615d673687482e4dedf5bd012ce3
agnieszkas <agnieszkas@users.noreply.github.com>
2019-12-07 14:58:59 +0100

    Operacje konflikt

---
+++ operacje_na_tekscie.py
@@ -0,0 +1,14 @@
+def capitalize(param):
+    return param.capitalize()
+
+def lower(param):
+    return param.islower()
+
+def count (param, param1):
+    return param.count(param1)
+
+def find(param, param1):
+    return param.find(param1)
+
+def isdigit(param):
+    return param.isdigit()
