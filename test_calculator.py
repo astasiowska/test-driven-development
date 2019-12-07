fb9f4cca310f811883c3916bf38f49638dbc208c
agnieszkas <agnieszkas@users.noreply.github.com>
2019-12-07 12:43:05 +0100

    TEDD3

--- test_calculator.py
+++ test_calculator.py
@@ -6,8 +6,19 @@ class TestCalculator(unittest.TestCase):
         wynik = calculator.dodaj(1, 3)
         self.assertEqual(wynik, 4)
 
-    def zwroc_parametr(self):
-        self.assertEqual(mojprogram.zwroc_parametr(124), 124)
+    def test_odejmij(self):
+        wynik = calculator.odejmij(10, 6)
+        self.assertEqual(wynik, 4)
+
+    def test_pomnoz(self):
+        wynik = calculator.pomnoz(10, 6)
+        self.assertEqual(wynik, 60)
+
+    def test_podziel(self):
+        wynik = calculator.podziel(10, 5)
+        self.assertEqual(wynik, 2)
+
+
 
 
 if __name__ == '__main__':
