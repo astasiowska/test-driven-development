52296396418d9bff8d043fe22eb8dfb938851dac
klecybyl <klecybyl@users.noreply.github.com>
2019-12-07 14:42:30 +0100

    zmiany Kasi

---
+++ operacje_na_tekscie.py
@@ -0,0 +1,14 @@
+def capitalize(txt):
+    return txt.capitalize()
+
+def lower(txt):
+    return txt.lower()
+
+def casefold(txt):
+    return str(txt).casefold()
+
+def upper(txt):
+    return txt.upper()
+
+def title(txt):
+    return txt.title()
